b437303a55bf5d28ed641a49c638008bb5832a37
Roman <stomp911@yandex.ru>
2022-05-17 12:26:44 -0700

    Lesson 1. API

--- authnapp/serializers.py
+++ authnapp/serializers.py
@@ -6,4 +6,5 @@ from .models import User
 class UserModelSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        # fields = '__all__'
+        fields = ['first_name', 'last_name', 'email']
